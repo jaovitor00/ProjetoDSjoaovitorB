6a590f960b8b78acbcee62d3d02a26c4f2656494
jaovitor00 <vitorjo.bernardo@gmail.com>
2025-04-22 11:32:40 -0300

    BANCO DE DADOS-LIVRARIA
    Atividade avaliativa

---
+++ bdlivraria.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 22-Abr-2025 às 16:27
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bdlivraria`
+--
+CREATE DATABASE IF NOT EXISTS `bdlivraria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `bdlivraria`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE `cliente` (
+  `cpf` varchar(20) NOT NULL,
+  `nome` varchar(25) NOT NULL,
+  `endereco` varchar(20) NOT NULL,
+  `cidade` varchar(20) NOT NULL,
+  `idade` varchar(3) NOT NULL,
+  `telefone` varchar(12) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livro`
+--
+
+DROP TABLE IF EXISTS `livro`;
+CREATE TABLE `livro` (
+  `isbn` varchar(20) NOT NULL,
+  `nome` varchar(40) NOT NULL,
+  `tipo` varchar(15) NOT NULL,
+  `valor` varchar(3) NOT NULL,
+  `datapubli` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `login` varchar(20) NOT NULL,
+  `senha` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cpf`);
+
+--
+-- Índices para tabela `livro`
+--
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`isbn`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`login`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
